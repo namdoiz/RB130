929e4615ae4ca1df1ffc23c27f541a088653dc7f
namdoiz <nnamdi.on7@gmail.com>
2022-11-10 00:31:23 -0600

    my custom none? method for arrays

---
+++ custom_none_method.rb
@@ -0,0 +1,17 @@
+def none?(array)
+  false_count = 0
+  array.each do |item|
+    return false if yield(item)
+    false_count += 1 unless yield(item)
+  end
+  return true if false_count == array.size
+  false
+end
+
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
